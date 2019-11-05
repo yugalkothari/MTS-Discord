0f1f6dbe4f0de5ba00643ecddec6f71595d7a23d
Yugal <yugal@MAC-QA-YUGAL.local>
2019-11-05 23:21:57 +0530

    persistance added

--- index.js
+++ index.js
@@ -3,23 +3,38 @@ const client = new Discord.Client()
 const request = require('request');
 let prefix = '!';
 let searchWrd = "";
-let googKey = "AIzaSyBjkyiGVlEGkyPgLfgRrq5G-bjp8_t3iUM";
-let cxKey = "003996562640305804704:lh63vr4eppq"
+let googKey = "";
+let cxKey = ""
+var url = "";
+var MongoClient = require('mongodb').MongoClient;
 
 
-client.login('NjM3MzEwOTY5NzQzNDA5MTYy.XbMUow.ZoGjX8mNwzx9nqsekV7uUhf8Cfg')
-
+client.login('')
 client.on('ready', () => {
     console.log('I am ready!');
 });
-
 
 client.on('message', message => {
     if (message.content === 'hi') {
         message.reply('hey')
     } else if (message.content.startsWith(prefix + "google")) {
-         message.reply('hold on while i search!!!')
+        message.reply('hold on while i search!!!')
         var searchWrd = message.toString().substr(8);
+        MongoClient.connect(url, function(err, db) {
+            if (err) throw err;
+            var dbo = db.db("searches");
+            var myobj = {
+                searchString: searchWrd
+            };
+            dbo.collection("searchStrings").insertOne(myobj, function(err, res) {
+                if (err) throw err;
+                console.log("1 document inserted");
+                dbo.createIndex({
+                    searchString: "text"
+                })
+                db.close();
+            });
+        });
         request("https://www.googleapis.com/customsearch/v1?key=" + googKey + "&cx=" + cxKey + "&q=" + searchWrd, function(err, res, body) {
             let data;
 
@@ -44,14 +59,32 @@ client.on('message', message => {
                 let result = data.items[i];
                 message.channel.sendMessage(result.title + '\n' + result.link);
             }
-
-
         });
-    } else {
-        //message.reply( "I am not sure what you are looking for kindly try to send me hi or use my google search capabilities by !google <to search for>");
+    } else if (message.content.startsWith(prefix + "recent")) {
+        var searchWrd = message.toString().substr(8);
+        MongoClient.connect(url, function(err, db) {
+            if (err) throw err;
+            var dbo = db.db("searches");
+            dbo.collection("searchStrings").find({
+                searchString: {
+                    $regex: searchWrd
+                }
+            }).toArray(function(err, result) {
+                if (err) throw err;
+                console.log(result)
+                if (!result[0].searchString) {
+                    message.reply('You never searched anything like this before on google.try searching by !google <thing to search for>')
+                } else {
+                    message.reply('I found your last five related searches.')
+                    for (var i = 0; i < 5; ++i) {
+                        console.log(result)
+                        message.channel.sendMessage(result[i].searchString);
+                    }
+                }
+                db.close();
+            });
+        });
+
     }
-
-
-
 
 })
